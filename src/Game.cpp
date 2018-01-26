a2152af1db01315ad89f96aabe1aa1b7c5de4eac
Xywzel <oskari.nousiainen@iki.fi>
2018-01-26 21:19:52 +0200

    Window opens

--- src/Game.cpp
+++ src/Game.cpp
@@ -14,14 +14,23 @@ bool Game::init()
 {
 	if(SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		std::cout << "Error: SDL_ could not be initialized. SDL_Error" << SDL_GetError() << std::endl;
+		std::cout << "Error: SDL could not be initialized. SDL_Error " << SDL_GetError() << std::endl;
 		return false;
 	}
 	window = SDL_CreateWindow(gameTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+	if(!window)
+	{
+		std::cout << "Error: SDL could not create window, SDL_Error " << SDL_GetError() << std::endl;
+		return false;
+	}
+	surface = SDL_GetWindowSurface(window);
+	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+	SDL_UpdateWindowSurface(window);
 
 	return true;
 }
 
 void Game::run()
 {
+	SDL_Delay(2000);
 }
