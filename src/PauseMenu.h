3d8dcbc5ccdff77909bc91824fdc502bc3f23e76
Xywzel <oskari.nousiainen@iki.fi>
2018-01-27 21:32:10 +0200

    Pause Menu

--- src/PauseMenu.h
+++ src/PauseMenu.h
@@ -2,16 +2,30 @@
 
 #include "Menu.h"
 
+#include "LTexture.h"
+
 class PauseMenu : public Menu
 {
 	public:
 		PauseMenu();
 		virtual ~PauseMenu();
 		virtual void render(SDL_Renderer* renderer);
+		virtual void handleEvent(SDL_Event& e);
 		virtual void update();
 		virtual void show(int arg = 0);
 		bool shouldExit();
 	private:
 		int score;
+		
+		enum Button{
+			MenuButton,
+			ContinueButton,
+		};
+		Button current;
+
+		LTexture background;
+		LTexture menuButton;
+		LTexture continueButton;
+		LTexture selection;
 };
 
