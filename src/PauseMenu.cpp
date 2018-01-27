ad678c47960a29bc1dde02dee6fd67e768f06093
Xywzel <oskari.nousiainen@iki.fi>
2018-01-27 16:17:49 +0200

    Pause and score fix

--- src/PauseMenu.cpp
+++ src/PauseMenu.cpp
@@ -1,4 +1,27 @@
 #include "PauseMenu.h"
+#include <SDL2/SDL.h>
+
+PauseMenu::PauseMenu()
+{
+}
+
+PauseMenu::~PauseMenu()
+{
+}
+
+void PauseMenu::renderer(SDL_Renderer* renderer)
+{
+}
+
+void PauseMenu::update()
+{
+}
+
+void PauseMenu::show(int arg)
+{
+	score =  arg;
+	Menu::show();
+}
 
 bool PauseMenu::shouldExit()
 {
--- src/PauseMenu.h
+++ src/PauseMenu.h
@@ -7,7 +7,7 @@ class PauseMenu : public Menu
 	public:
 		PauseMenu();
 		virtual ~PauseMenu();
-		virtual void render();
+		virtual void render(SDL_Renderer* renderer);
 		virtual void update();
 		virtual void show(int arg = 0);
 		bool shouldExit();
